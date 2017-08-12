72a9312f0ad9275122c8c3fa8e5484b3063ba32f
Orozco <luis.fernando.orozco@accenture.com>
2017-08-11 19:31:34 -0500

    Commit inicial

---
+++ src/com/udea/test/PruebaTemp.java
@@ -0,0 +1,25 @@
+package com.udea.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PruebaTemp {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2(){
+		assertTrue(true);
+	}
+
+}
