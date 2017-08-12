3660b6c37729e70ee3064333f4f43a0451cc51ad
adrian.martinez <adrian.martinez@udea.edu.co>
2017-08-11 20:21:11 -0500

    Edicion de la lista

--- src/com/udea/lista/ListaLigada.java
+++ src/com/udea/lista/ListaLigada.java
@@ -3,19 +3,53 @@ package com.udea.lista;
 public class ListaLigada 
 {
 	public Nodo cabeza;
-	
+	public int	cantidad;
 	public Nodo ultimo;
-	
+
 	public ListaLigada(float n)
 	{
 		cabeza = new Nodo(n);
 		ultimo = cabeza;
+		cantidad = 1;
 	}
 	
+	public ListaLigada()
+	{
+		cabeza = null;
+		ultimo = cabeza;
+		cantidad = 0;
+	}
+
 	public void AgregarNodo(float n)
 	{
 		Nodo nNodo = new Nodo(n);
 		nNodo.AsignarSiguiente(ultimo);
-		ultimo.AsignarSiguiente(nNodo);
+		if (cantidad == 0)
+		{
+			cabeza = nNodo;
+		}else{
+			ultimo.AsignarSiguiente(nNodo);
+		}
+		
+		cantidad ++;
 	}
+	
+	public float RetornarSuma()
+	{
+		float resultado = cabeza.valor;
+		Nodo flotante = cabeza;
+		do {
+			flotante 	 = 	flotante.siguiente;
+			resultado 	+= 	flotante.valor;
+		}while(flotante!=ultimo);
+		
+		return resultado;
+	}
+	
+	public int RetornarCantidad()
+	{
+		return cantidad;
+	}
+	
+	
 }
